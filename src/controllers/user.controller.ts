16043ab84fbdcc4d6688b349ed4d601aa963ce2c
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2025-09-29 22:38:45 -0300

    feat: :sparkles: Implement CRUD operations for user management in controller and routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -29,3 +29,96 @@ export async function store(
     return next(err);
   }
 }
+
+export async function index(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const users = await UserRepository.findAll();
+
+    return res.json(success('Users retrieved successfully', users));
+  } catch (err) {
+    return next(err);
+  }
+}
+
+export async function show(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const userId = parseInt(req.params.id, 10);
+    if (isNaN(userId) || userId <= 0) {
+      throw new CustomError("Invalid user ID", HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const user = await UserRepository.findById(userId);
+    if (!user) {
+      throw new CustomError("User not found", HTTP_STATUS.NOT_FOUND);
+    }
+
+    return res.json(success('User retrieved successfully', user));
+  } catch (err) {
+    return next(err);
+  }
+}
+
+export async function update(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const userId = parseInt(req.params.id, 10);
+    if (isNaN(userId) || userId <= 0) {
+      throw new CustomError("Invalid user ID", HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const userData: UserRequest = userSchema(true).parse(req.body);
+
+    if (userData.email) {
+      const existingUser = await UserRepository.findByEmail(userData.email);
+
+      if (existingUser && existingUser.id !== userId) {
+        throw new CustomError("Email already in use", HTTP_STATUS.CONFLICT);
+      }
+    }
+
+    if (userData.password) {
+      userData.password = await hashPassword(userData.password);
+    }
+
+    const updatedUser = await UserRepository.updateUser(userId, userData);
+
+    return res.json(success('User updated successfully', updatedUser));
+  } catch (err) {
+    return next(err);
+  }
+}
+
+export async function destroy(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const userId = parseInt(req.params.id, 10);
+    if (isNaN(userId) || userId <= 0) {
+      throw new CustomError("Invalid user ID", HTTP_STATUS.BAD_REQUEST);
+    }
+
+    const user = await UserRepository.findById(userId);
+    if (!user) {
+      throw new CustomError("User not found", HTTP_STATUS.NOT_FOUND);
+    }
+
+    await UserRepository.deleteUser(userId);
+
+    return res.json(success('User deleted successfully'));
+  } catch (err) {
+    return next(err);
+  }
+}
